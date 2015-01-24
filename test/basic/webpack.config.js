a0c856a01ba40e9ca34a5ac2e52ca74dac855eab
James Brantly <james@jbrantly.com>
2015-01-24 15:47:31 -0500

    Change test output to be ignored by git and npm

--- test/basic/webpack.config.js
+++ test/basic/webpack.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     context: __dirname,
     entry: './app.ts',
+    output: {
+        path: __dirname
+    },
     resolve: {
         alias: { externalLib: "./lib/externalLib.js" },
         extensions: ['', '.js', '.ts']
