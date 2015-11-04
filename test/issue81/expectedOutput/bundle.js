a49d44faedbda5be04bd336893594c8f25f01554
James Brantly <james@jbrantly.com>
2015-11-04 08:10:57 -0500

    Minor update to test due to change in Babel output

--- test/issue81/expectedOutput/bundle.js
+++ test/issue81/expectedOutput/bundle.js
@@ -53,7 +53,7 @@
 	        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
 	        d;
 	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-	    return (c > 3 && r && Object.defineProperty(target, key, r), r);
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
 	};
 	function bar(target) {
 	    return target;
