b0e49c6d683f14ebb2a228f1749e6a7601a07e93
John Reilly <johnny_reilly@hotmail.com>
2016-10-09 06:16:54 +0100

    Exclude ' [built]' from comparisons As it appears to be irrelevant and causes certain tastes to 'fail' when there's actually nothing wrong Allow running single tests and saving output Slight refactor to run.js (and code reformatting hence the big diff)

--- test/run.js
+++ test/run.js
@@ -39,48 +39,38 @@ var stagingPath = path.resolve(rootPath, '.test');
 rimraf.sync(stagingPath);
 
 // loop through each test directory
-fs.readdirSync(__dirname).forEach(function(test) {
+fs.readdirSync(__dirname).forEach(function (test) {
     var testPath = path.join(__dirname, test);
     if (fs.statSync(testPath).isDirectory()) {
-        
+
         if (test == 'testLib') return;
-        
+
         if (test == 'issue81' && semver.lt(typescript.version, '1.7.0-0')) return;
-        
+
         if (singleTestToRun && singleTestToRun !== test) return;
 
-        describe(test, function() {
+        describe(test, function () {
             it('should have the correct output', createTest(test, testPath, {}));
-            
+
             if (test == 'declarationOutput') { return; }
             if (test == 'declarationWatch') { return; }
             if (test == 'issue71') { return; }
-            it('should work with transpile', createTest(test, testPath, {transpile: true}));
+            it('should work with transpile', createTest(test, testPath, { transpile: true }));
         });
     }
 });
 
-function pathExists(path) {
-    var pathExists = true;
-    try {
-        fs.accessSync(path, fs.F_OK);
-    } catch (e) {
-        pathExists = false;
-    }
-    return pathExists;
-}
-
 function createTest(test, testPath, options) {
-    return function(done) {
+    return function (done) {
         this.timeout(60000); // sometimes it just takes awhile
-        
+
         // set up paths
-        var testStagingPath = path.join(stagingPath, test+(options.transpile ? '.transpile' : '')),
+        var testStagingPath = path.join(stagingPath, test + (options.transpile ? '.transpile' : '')),
             actualOutput = path.join(testStagingPath, 'actualOutput'),
-            expectedOutput = path.join(testStagingPath, 'expectedOutput-'+typescriptVersion),
+            expectedOutput = path.join(testStagingPath, 'expectedOutput-' + typescriptVersion),
             webpackOutput = path.join(testStagingPath, '.output'),
-            originalExpectedOutput = path.join(testPath, 'expectedOutput-'+typescriptVersion);
-        
+            originalExpectedOutput = path.join(testPath, 'expectedOutput-' + typescriptVersion);
+
         if (saveOutputMode) {
             savedOutputs[test] = savedOutputs[test] || {};
             var regularSavedOutput = savedOutputs[test].regular = savedOutputs[test].regular || {};
@@ -90,16 +80,16 @@ function createTest(test, testPath, options) {
         } else {
             assert.ok(pathExists(originalExpectedOutput), 'The expected output does not exist; there is nothing to compare against! Has the expected output been created?\nCould not find: ' + originalExpectedOutput)
         }
-        
+
         // copy all input to a staging area
         mkdirp.sync(testStagingPath);
         fs.copySync(testPath, testStagingPath);
-           
-            
+
+
         // ensure output directories
         mkdirp.sync(actualOutput);
         mkdirp.sync(webpackOutput);
-        
+
         // execute webpack
         var config = require(path.join(testStagingPath, 'webpack.config'));
         config.output.path = webpackOutput;
@@ -113,70 +103,70 @@ function createTest(test, testPath, options) {
         config.ts.compilerOptions = {
             newLine: 'LF'
         }
-        
+
         if (options.transpile) config.ts.transpileOnly = true;
-        
+
         var iteration = 0;
         var lastHash;
-        var watcher = webpack(config).watch({aggregateTimeout: 1500}, function(err, stats) {
+        var watcher = webpack(config).watch({ aggregateTimeout: 1500 }, function (err, stats) {
             var patch = '';
             if (iteration > 0) {
-                patch = 'patch'+(iteration-1);
+                patch = 'patch' + (iteration - 1);
                 actualOutput = path.join(testStagingPath, 'actualOutput', patch);
-                expectedOutput = path.join(testStagingPath, 'expectedOutput-'+typescriptVersion, patch);
-                originalExpectedOutput = path.join(testPath, 'expectedOutput-'+typescriptVersion, patch)
+                expectedOutput = path.join(testStagingPath, 'expectedOutput-' + typescriptVersion, patch);
+                originalExpectedOutput = path.join(testPath, 'expectedOutput-' + typescriptVersion, patch)
                 mkdirp.sync(actualOutput);
                 mkdirp.sync(expectedOutput);
                 if (saveOutputMode) mkdirp.sync(originalExpectedOutput);
             }
-            
+
             // replace the hash if found in the output since it can change depending
             // on environments and we're not super interested in it
             if (stats) {
-                glob.sync('**/*', {cwd: webpackOutput, nodir: true}).forEach(function(file) {
+                glob.sync('**/*', { cwd: webpackOutput, nodir: true }).forEach(function (file) {
                     var content = fs.readFileSync(path.join(webpackOutput, file), 'utf-8');
                     content = content.split(stats.hash).join('[hash]');
                     fs.writeFileSync(path.join(webpackOutput, file), content);
                 });
             }
-            
+
             // output results
             if (saveOutputMode) {
                 // loop through webpackOutput and rename to .transpiled if needed
-                glob.sync('**/*', {cwd: webpackOutput, nodir: true}).forEach(function(file) {
-                    var patchedFileName = patch+'/'+file;
+                glob.sync('**/*', { cwd: webpackOutput, nodir: true }).forEach(function (file) {
+                    var patchedFileName = patch + '/' + file;
                     currentSavedOutput[patchedFileName] = fs.readFileSync(path.join(webpackOutput, file), 'utf-8');
-                    
+
                     if (options.transpile) {
                         if (regularSavedOutput[patchedFileName] !== transpiledSavedOutput[patchedFileName]) {
                             var extension = path.extname(file);
                             fs.renameSync(
-                                path.join(webpackOutput, file), 
-                                path.join(webpackOutput, path.basename(file, extension)+'.transpiled'+extension)
+                                path.join(webpackOutput, file),
+                                path.join(webpackOutput, path.basename(file, extension) + '.transpiled' + extension)
                             );
                         }
                     }
                 });
-                
+
                 fs.copySync(webpackOutput, originalExpectedOutput, { clobber: true });
             }
             fs.copySync(webpackOutput, actualOutput);
             rimraf.sync(webpackOutput);
-            
+
             if (err) {
                 var errFileName = 'err.txt';
-                
+
                 var errString = err.toString()
-                    .replace(new RegExp(regexEscape(testStagingPath+path.sep), 'g'), '')
-                    .replace(new RegExp(regexEscape(rootPath+path.sep), 'g'), '')
+                    .replace(new RegExp(regexEscape(testStagingPath + path.sep), 'g'), '')
+                    .replace(new RegExp(regexEscape(rootPath + path.sep), 'g'), '')
                     .replace(new RegExp(regexEscape(rootPath), 'g'), '')
                     .replace(/\.transpile/g, '');
-                
+
                 fs.writeFileSync(path.join(actualOutput, errFileName), errString);
                 if (saveOutputMode) {
-                    var patchedErrFileName = patch+'/'+errFileName;
+                    var patchedErrFileName = patch + '/' + errFileName;
                     currentSavedOutput[patchedErrFileName] = errString;
-                    
+
                     if (options.transpile) {
                         if (regularSavedOutput[patchedErrFileName] !== transpiledSavedOutput[patchedErrFileName]) {
                             fs.writeFileSync(path.join(originalExpectedOutput, 'err.transpiled.txt'), errString);
@@ -187,31 +177,31 @@ function createTest(test, testPath, options) {
                     }
                 }
             }
-            
+
             if (stats && stats.hash != lastHash) {
                 lastHash = stats.hash;
-                
+
                 var statsFileName = 'output.txt';
-                
+
                 // do a little magic to normalize `\` to `/` for asset output
                 var newAssets = {};
-                Object.keys(stats.compilation.assets).forEach(function(asset) { 
-                    newAssets[asset.replace(/\\/g, "/")] = stats.compilation.assets[asset]; 
+                Object.keys(stats.compilation.assets).forEach(function (asset) {
+                    newAssets[asset.replace(/\\/g, "/")] = stats.compilation.assets[asset];
                 });
                 stats.compilation.assets = newAssets;
-                
-                var statsString = stats.toString({timings: false, version: false, hash: false})
-                    .replace(new RegExp(regexEscape(testStagingPath+path.sep), 'g'), '')
-                    .replace(new RegExp(regexEscape(rootPath+path.sep), 'g'), '')
+
+                var statsString = stats.toString({ timings: false, version: false, hash: false })
+                    .replace(new RegExp(regexEscape(testStagingPath + path.sep), 'g'), '')
+                    .replace(new RegExp(regexEscape(rootPath + path.sep), 'g'), '')
                     .replace(new RegExp(regexEscape(rootPath), 'g'), '')
                     .replace(new RegExp(regexEscape(rootPathWithIncorrectWindowsSeparator), 'g'), '')
                     .replace(/\.transpile/g, '');
-                
+
                 fs.writeFileSync(path.join(actualOutput, statsFileName), statsString);
                 if (saveOutputMode) {
-                    var patchedStatsFileName = patch+'/'+statsFileName;
+                    var patchedStatsFileName = patch + '/' + statsFileName;
                     currentSavedOutput[patchedStatsFileName] = statsString;
-                    
+
                     if (options.transpile) {
                         if (regularSavedOutput[patchedStatsFileName] !== transpiledSavedOutput[patchedStatsFileName]) {
                             fs.writeFileSync(path.join(originalExpectedOutput, 'output.transpiled.txt'), statsString);
@@ -222,16 +212,16 @@ function createTest(test, testPath, options) {
                     }
                 }
             }
-        
+
             if (!saveOutputMode) {
                 // massage any .transpiled. files
-                glob.sync('**/*', {cwd: expectedOutput, nodir: true}).forEach(function(file) {
+                glob.sync('**/*', { cwd: expectedOutput, nodir: true }).forEach(function (file) {
                     if (/\.transpiled/.test(file)) {
                         if (options.transpile) { // rename if we're in transpile mode
                             var extension = path.extname(file);
                             fs.renameSync(
-                                path.join(expectedOutput, file), 
-                                path.join(expectedOutput, path.dirname(file), path.basename(file, '.transpiled'+extension)+extension)
+                                path.join(expectedOutput, file),
+                                path.join(expectedOutput, path.dirname(file), path.basename(file, '.transpiled' + extension) + extension)
                             );
                         }
                         else { // otherwise delete
@@ -240,61 +230,88 @@ function createTest(test, testPath, options) {
 
                     }
                 });
-                
+
                 // compare actual to expected
-                var actualFiles = glob.sync('**/*', {cwd: actualOutput, nodir: true}),
-                    expectedFiles = glob.sync('**/*', {cwd: expectedOutput, nodir: true})
-                        .filter(function(file) { return !/^patch/.test(file); }),
+                var actualFiles = glob.sync('**/*', { cwd: actualOutput, nodir: true }),
+                    expectedFiles = glob.sync('**/*', { cwd: expectedOutput, nodir: true })
+                        .filter(function (file) { return !/^patch/.test(file); }),
                     allFiles = {};
-                        
-                actualFiles.map(function(file) { allFiles[file] = true });
-                expectedFiles.map(function(file) { allFiles[file] = true });
-                
-                Object.keys(allFiles).forEach(function(file) {
-                    try {
-                        var actual = fs.readFileSync(path.join(actualOutput, file)).toString().replace(/\r\n/g, '\n');
-                    }
-                    catch (e) { actual = '!!!actual file doesnt exist!!!' }
-                    
-                    try {
-                        var expected = fs.readFileSync(path.join(expectedOutput, file)).toString().replace(/\r\n/g, '\n');
-                    }
-                    catch (e) { expected = '!!!expected file doesnt exist!!!' }
-                    
-                    // If a test is marked as flaky then don't fail the build if it doesn't pass
-                    // Report the differences and carry on
-                    if (test.indexOf("_FLAKY_") === 0) {
-                        try {
-                            assert.equal(actual.toString(), expected.toString(), (patch?patch+'/':patch) + file + ' is different between actual and expected');
-                        }
-                        catch (e) {
-                            console.log("Flaky test error!\n");
-                            console.log("MESSAGE:\n" + e.message, '\n');
-                            console.log('EXPECTED:\n', e.expected, '\n');
-                            console.log("ACTUAL:\n", e.actual, '\n');
-                        }
-                    }
-                    else {
-                        assert.equal(actual.toString(), expected.toString(), (patch?patch+'/':patch) + file + ' is different between actual and expected');
-                    }
+
+                actualFiles.map(function (file) { allFiles[file] = true });
+                expectedFiles.map(function (file) { allFiles[file] = true });
+
+                Object.keys(allFiles).forEach(function (file) {
+                    var actual = getNormalisedFileContent(file, actualOutput);
+                    var expected = getNormalisedFileContent(file, expectedOutput);
+
+                    compareActualAndExpected(test, actual, expected, patch, file);
                 });
             }
-            
+
             // check for new files to copy in
-            var patchPath = path.join(testStagingPath, 'patch'+iteration);
+            var patchPath = path.join(testStagingPath, 'patch' + iteration);
             if (fs.existsSync(patchPath)) {
                 iteration++;
-                
+
                 // can get inconsistent results if copying right away
-                setTimeout(function() {
-                    fs.copySync(patchPath, testStagingPath, {clobber: true});
+                setTimeout(function () {
+                    fs.copySync(patchPath, testStagingPath, { clobber: true });
                 }, 1000);
             }
             else {
-                watcher.close(function() {
+                watcher.close(function () {
                     done();
                 });
             }
         });
     };
+}
+
+function pathExists(path) {
+    var pathExists = true;
+    try {
+        fs.accessSync(path, fs.F_OK);
+    } catch (e) {
+        pathExists = false;
+    }
+    return pathExists;
+}
+
+function getNormalisedFileContent(file, location) {
+    var fileContent;
+    var filePath = path.join(location, file);
+    try {
+        fileContent = fs.readFileSync(filePath).toString().replace(/\r\n/g, '\n');
+
+        // Strip ' [built]' references from output*.txt files; seems to be used unpredictably 
+        // and doesn't appear to be relevant so safe to ignore
+        if (file.indexOf('output') === 0) {
+            fileContent = fileContent.replace(new RegExp(regexEscape(' [built]'), 'g'), '');
+        }
+    }
+    catch (e) {
+        fileContent = '!!!' + filePath + ' doesnt exist!!!';
+    }
+    return fileContent;
+}
+
+/**
+ * If a test is marked as flaky then don't fail the build if it doesn't pass
+ * Instead, report the differences and carry on
+ */
+function compareActualAndExpected(test, actual, expected, patch, file) {
+    if (test.indexOf("_FLAKY_") === 0) {
+        try {
+            assert.equal(actual.toString(), expected.toString(), (patch ? patch + '/' : patch) + file + ' is different between actual and expected');
+        }
+        catch (e) {
+            console.log("Flaky test error!\n");
+            console.log("MESSAGE:\n" + e.message, '\n');
+            console.log('EXPECTED:\n', e.expected, '\n');
+            console.log("ACTUAL:\n", e.actual, '\n');
+        }
+    }
+    else {
+        assert.equal(actual.toString(), expected.toString(), (patch ? patch + '/' : patch) + file + ' is different between actual and expected');
+    }
 }
