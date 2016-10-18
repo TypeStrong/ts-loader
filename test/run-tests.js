3f5f19e76f58516c1390271da4d7aab627f19ba6
John Reilly <johnny_reilly@hotmail.com>
2016-10-18 11:01:29 +0100

    make comparison-tests run before execution-tests
    because declarationWatch occasionally errors on Windows when:
    - both test suites are run (`npm test`)
    - when execution-tests run before comparison-tests
    
    Why?  Well why indeed.

--- test/run-tests.js
+++ test/run-tests.js
@@ -7,5 +7,5 @@ var execSync = require('child_process').execSync;
 console.log('Using webpack version ' + webpackVersion);
 console.log('Using typescript version ' + typescript.version);
 
-execSync('npm run execution-tests', { stdio: 'inherit' });
 execSync('npm run comparison-tests', { stdio: 'inherit' });
+execSync('npm run execution-tests', { stdio: 'inherit' });
