bbbf55bd8066bc30e2c84c7fc7d45d4ec646f2a3
John Reilly <johnny_reilly@hotmail.com>
2016-11-13 15:07:31 +0000

    Start running test pack against 2.1.1 (#371)
    * Start running test pack against 2.1.1
    
    * forgot like a crucial thing
    
    * regenerate test data for 2.1

--- test/comparison-tests/stub-new-version.js
+++ test/comparison-tests/stub-new-version.js
@@ -6,7 +6,7 @@ fs.readdirSync(__dirname).forEach(function(test) {
     var testPath = path.join(__dirname, test);
     if (fs.statSync(testPath).isDirectory()) {
         
-        if (test == 'testLib') return;
+        if (test === 'testLib') return;
         
         // update this manually when a new version comes out
         var expectedOutput = path.join(testPath, 'expectedOutput-2.0'),
