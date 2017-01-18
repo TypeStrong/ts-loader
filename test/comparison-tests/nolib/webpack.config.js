02a7f439f54901fc8857f88f4da2223eec86017e
John Reilly <johnny_reilly@hotmail.com>
2017-01-18 21:49:26 +0000

    nolib

--- test/comparison-tests/nolib/webpack.config.js
+++ test/comparison-tests/nolib/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         extensions: ['.ts', '.js']
     },
     module: {
-        loaders: [
+        rules: [
             { test: /\.ts$/, loader: 'ts-loader' }
         ]
     }
