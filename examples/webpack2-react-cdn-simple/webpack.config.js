6861e0e90b3839b114ab1570afcb53bc1eaf9217
Tom Cumming <mail@tomcumming.co.uk>
2017-01-29 12:44:08 +0000

    Added a simple example using webpack2, react and JSX without using babel (#455)

---
+++ examples/webpack2-react-cdn-simple/webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: { filename: 'index.js' },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.tsx' ]
+    },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM'
+    }
+};
