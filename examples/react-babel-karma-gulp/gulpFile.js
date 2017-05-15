f27b313e5718c0f961c49f97f835ad45a3b9c459
John Reilly <johnny_reilly@hotmail.com>
2017-05-15 20:38:56 +0100

    re-organise examples

--- examples/react-babel-karma-gulp/gulpFile.js
+++ examples/react-babel-karma-gulp/gulpFile.js
@@ -1,30 +1,22 @@
-/* eslint-disable no-var, strict, prefer-arrow-callback */
 'use strict';
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var eslint = require('gulp-eslint');
 var webpack = require('./gulp/webpack');
 var staticFiles = require('./gulp/staticFiles');
 var tests = require('./gulp/tests');
 var clean = require('./gulp/clean');
 var inject = require('./gulp/inject');
 
-var lintSrcs = ['./gulp/**/*.js'];
-
 gulp.task('delete-dist', function (done) {
   clean.run(done);
 });
 
-gulp.task('build-process.env.NODE_ENV', function () {
-  process.env.NODE_ENV = 'production';
-});
-
-gulp.task('build-js', ['delete-dist', 'build-process.env.NODE_ENV'], function(done) {
+gulp.task('build-js', ['delete-dist'], function(done) {
   webpack.build().then(function() { done(); });
 });
 
-gulp.task('build-other', ['delete-dist', 'build-process.env.NODE_ENV'], function() {
+gulp.task('build-other', ['delete-dist'], function() {
   staticFiles.build();
 });
 
@@ -32,17 +24,9 @@ gulp.task('build', ['build-js', 'build-other', 'lint'], function () {
   inject.build();
 });
 
-gulp.task('lint', function () {
-  return gulp.src(lintSrcs)
-    .pipe(eslint())
-    .pipe(eslint.format());
-});
-
 gulp.task('watch', ['delete-dist'], function(done) {
-  process.env.NODE_ENV = 'development';
   Promise.all([
-    webpack.watch()//,
-    //less.watch()
+    webpack.watch()
   ]).then(function() {
     gutil.log('Now that initial assets (js and css) are generated inject will start...');
     inject.watch();
@@ -51,7 +35,6 @@ gulp.task('watch', ['delete-dist'], function(done) {
     gutil.log('Problem generating initial assets (js and css)', error);
   });
 
-  gulp.watch(lintSrcs, ['lint']);
   staticFiles.watch();
   tests.watch();
 });
