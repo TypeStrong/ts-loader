130f128cac2a520f33c90e70ec101e3316f65c8f
John Reilly <johnny_reilly@hotmail.com>
2017-05-15 21:12:03 +0100

    Added more complex ts-checker example

---
+++ examples/fork-ts-checker-react-babel-karma-gulp/src/components/Greeting.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import * as GreetingActions from '../actions/GreetingActions';
+
+interface Props {
+  key: number;
+  targetOfGreeting: string;
+}
+
+class Greeting extends React.Component<Props, any> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p>
+        Hello { this.props.targetOfGreeting }!
+
+        <button className="btn btn-default btn-danger"
+                onClick={ this._onClick }>
+                Remove
+        </button>
+      </p>
+    );
+  }
+
+  _onClick = (_event: React.MouseEvent<HTMLButtonElement>) => {
+    GreetingActions.removeGreeting(this.props.targetOfGreeting);
+  }
+}
+
+export default Greeting;
