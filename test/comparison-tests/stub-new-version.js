7c7e7732b027f2cdde9975dad1f64ba7877fb128
John Reilly <johnny_reilly@hotmail.com>
2017-06-29 05:35:09 +0100

    Create 2.4 expected comparison test output based upon 2.3 output

--- test/comparison-tests/stub-new-version.js
+++ test/comparison-tests/stub-new-version.js
@@ -9,8 +9,8 @@ fs.readdirSync(__dirname).forEach(function(test) {
         if (test === 'testLib') return;
         
         // update this manually when a new version comes out
-        var expectedOutput = path.join(testPath, 'expectedOutput-2.2'),
-            newExpectedOutput = path.join(testPath, 'expectedOutput-2.3');
+        var expectedOutput = path.join(testPath, 'expectedOutput-2.3'),
+            newExpectedOutput = path.join(testPath, 'expectedOutput-2.4');
         
         mkdirp.sync(newExpectedOutput);
         fs.copySync(expectedOutput, newExpectedOutput);
