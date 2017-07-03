44bd7c6bd6f8e48aba5143bdcf5cab5b268538d4
John Reilly <johnny_reilly@hotmail.com>
2017-07-03 07:26:48 +0100

    Add promise import usage

--- test/execution-tests/2.4.1_babel-importCodeSplitting/src/app.ts
+++ test/execution-tests/2.4.1_babel-importCodeSplitting/src/app.ts
@@ -4,6 +4,10 @@ import b from './b';
 console.log(a);
 console.log(b);
 
+const d = import('./d').then(d => {
+    console.log(d.d);
+})
+
 async () => {
     const c = await import('./c');
     console.log(c.default);
