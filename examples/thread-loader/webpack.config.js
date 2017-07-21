1c1056f494622fc1ed5597a1c02c5c13b9d61230
John Reilly <johnny_reilly@hotmail.com>
2017-07-21 08:26:51 +0100

    Update webpack.config.js

--- examples/thread-loader/webpack.config.js
+++ examples/thread-loader/webpack.config.js
@@ -8,23 +8,25 @@ module.exports = {
     entry: './src/index.ts',
     output: { filename: 'dist/index.js' },
     module: {
-        rules: [
-            { loader: 'cache-loader' },
-            {
-                loader: 'thread-loader',
-                options: {
-                    // there should be 1 cpu for the fork-ts-checker-webpack-plugin
-                    workers: require('os').cpus().length - 1,
+        rules: {
+            test: /\.tsx?$/,
+            use: [
+                { loader: 'cache-loader' },
+                {
+                    loader: 'thread-loader',
+                    options: {
+                        // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+                        workers: require('os').cpus().length - 1,
+                    },
                 },
-            },
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                options: {
-                    happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+                {
+                    loader: 'ts-loader',
+                    options: {
+                        happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+                    }
                 }
-            }
-        ]
+            ]
+        }
     },
     resolve: {
         extensions: ['.ts', '.tsx', 'js']
