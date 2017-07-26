8c9ee4f700b97538324914351571406319b22358
John Reilly <john.reilly@baker-technology.ltd.uk>
2017-07-26 08:48:06 +0100

    correct test to reference entry

--- test/execution-tests/2.0.3_babel-allowJsImportTypes/test/entry.tests.js
+++ test/execution-tests/2.0.3_babel-allowJsImportTypes/test/entry.tests.js
@@ -2,7 +2,7 @@ var entry = require('../src/entry');
 
 describe("entry", function() {
   it("getEventRank produces something", function() {
-    var EventRank = getEventRank();
+    var EventRank = entry.getEventRank();
     expect(EventRank).not.toBeUndefined();
   });
 });
