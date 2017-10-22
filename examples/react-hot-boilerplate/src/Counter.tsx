e6203524ea679f0f25b6d91487fccb157c080a2f
John Reilly <johnny_reilly@hotmail.com>
2017-10-22 20:44:58 +0100

    updated deps

--- examples/react-hot-boilerplate/src/Counter.tsx
+++ examples/react-hot-boilerplate/src/Counter.tsx
@@ -4,7 +4,7 @@ export default class Counter extends React.Component<{}, { counter: number }> {
   interval: number;
   constructor(props: {}) {
     super(props);
-    this.state = { counter: 1080 };
+    this.state = { counter: 0 };
   }
 
   componentDidMount() {
