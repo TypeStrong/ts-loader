512c9e6cca8410d135f3e0abfe765257cd9ac68c
John Reilly <johnny_reilly@hotmail.com>
2017-11-04 06:40:46 +0000

    create ts 2.6 comparison-test results

---
+++ test/comparison-tests/codeSplitting/expectedOutput-2.6/0.bundle.js
@@ -0,0 +1,20 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+module.exports = 'c';
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+module.exports = 'd';
+
+
+/***/ })
+]);
