0951587ac6da4977158c27d32ed5578387d41a50
John Reilly <johnny_reilly@hotmail.com>
2017-11-04 07:03:21 +0000

    regenerate output for 2.6 and add 2.6 to test matrix

--- test/comparison-tests/run-tests.js
+++ test/comparison-tests/run-tests.js
@@ -7,7 +7,7 @@ var execSync = require('child_process').execSync;
 
 // We only want to run comparison tests for the latest released version
 var typescriptVersion = parseFloat(semver.major(typescript.version) + '.' + semver.minor(typescript.version));
-if (typescriptVersion < 2.5  || typescriptVersion > 2.5) return;
+if (typescriptVersion < 2.6  || typescriptVersion > 2.6) return;
 
 // Parse command line arguments
 var saveOutputMode = process.argv.indexOf('--save-output') !== -1;
