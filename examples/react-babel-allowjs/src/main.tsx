5a32a070c4ea6852d40fcdb55afa78cd7f0fa1ff
Tiago Rodrigues <tmcrodrigues@gmail.com>
2017-11-08 13:56:25 +0100

    Update example to actually include some typed code

--- examples/react-babel-allowjs/src/main.tsx
+++ examples/react-babel-allowjs/src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 
+function getContainer (id: string) {
+  return document.getElementById(id)
+}
+
 ReactDOM.render(
   <App />,
-  document.getElementById('appContainer')
+  getContainer('appContainer')
 );
-console.log(App)
