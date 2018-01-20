44fa12bae31de59283006316ec179e2de55d508a
John Reilly <johnny_reilly@hotmail.com>
2018-01-20 10:00:42 +0000

    create 2.7 test output

---
+++ test/comparison-tests/es6codeSplitting/expectedOutput-2.7/0.bundle.js
@@ -0,0 +1,22 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = 'c';
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = 'd';
+
+
+/***/ })
+]);
