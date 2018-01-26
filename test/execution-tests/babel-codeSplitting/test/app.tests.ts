df88632d69bbae279f1be66c8031093c32a063f3
John Reilly <johnny_reilly@hotmail.com>
2018-01-26 16:13:10 +0000

    fix babel-codeSplitting test

--- test/execution-tests/babel-codeSplitting/test/app.tests.ts
+++ test/execution-tests/babel-codeSplitting/test/app.tests.ts
@@ -1,14 +1,20 @@
 // This test has been ported from the comparison test pack
 // ... perhaps it belongs better there though. Will keep for now.
 
-import a = require("../src/a");
-import b = require("../src/b");
+import a from "../src/a";
+import b from "../src/b";
 // modules c and d won't actually be emitted as "require" calls here
 // since they are not used directly. Instead, they are only referenced
 // with "typeof". At this point, these statements are only for the
 // benefit of the TypeScript type system.
-import c = require("../src/c");
-import d = require("../src/d");
+import c from "../src/c";
+import d from "../src/d";
+
+declare var require: {
+  <T>(path: string): T;
+  (paths: string[], callback: (...modules: any[]) => void): void;
+  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
 
 describe("app", () => {
   it("a to be 'a' and b to be 'b'", () => {
@@ -22,12 +28,12 @@ describe("app", () => {
       // `import ... require` statements). `require.ensure` is defined in
       // require.d.ts. Webpack sees this and automatically puts c and d
       // into a separate chunk. 
-      var cModule = <typeof c>require("../src/c");
-      var dModule = <typeof d>require("../src/d");
+      var cModule = <{ default: typeof c}>require("../src/c");
+      var dModule = <{ default: typeof d}>require("../src/d");
       
       // cModule and dModule will typed as strings
-      expect(cModule).toBe("c");
-      expect(dModule).toBe("d");
+      expect(cModule.default).toBe("c");
+      expect(dModule.default).toBe("d");
 
       done();
     });
