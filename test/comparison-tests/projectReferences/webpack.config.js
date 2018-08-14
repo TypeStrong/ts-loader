615bcbb0d74e5aa65a9a954c255ea95f9be6b3b4
Andrew Branch <andrew@wheream.io>
2018-08-14 14:03:08 -0600

    Add comparison test for project references with source map (no warnings)

---
+++ test/comparison-tests/projectReferences/webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './app.ts',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+}
+
+
