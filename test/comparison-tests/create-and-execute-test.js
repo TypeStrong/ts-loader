217db51d2593c12fed21e95c0e678a880511f686
John Reilly <johnny_reilly@hotmail.com>
2018-09-27 23:14:27 +0100

    remove start of line from regex

--- test/comparison-tests/create-and-execute-test.js
+++ test/comparison-tests/create-and-execute-test.js
@@ -276,7 +276,7 @@ function storeStats(stats, testState, paths, outputs, patch, options) {
 
         const statsString = stats.toString({ timings: false, version: false, hash: false, builtAt: false })
             .replace(/^Built at: .+$/gm, '')
-            .replace(/^Module build failed \(from [\/|\\]/, 'Module build failed (from ')
+            .replace(/Module build failed \(from [\/|\\]/, 'Module build failed (from ')
             .replace(new RegExp(regexEscape(paths.testStagingPath + path.sep), 'g'), '')
             .replace(new RegExp(regexEscape(rootPath + path.sep), 'g'), '')
             .replace(new RegExp(regexEscape(rootPath), 'g'), '')
