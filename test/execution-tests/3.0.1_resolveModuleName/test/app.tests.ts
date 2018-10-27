289d904cfe99f19846b02f33f4205b13d68cb2fb
Maël Nison <nison.mael@gmail.com>
2018-10-27 19:18:00 +0100

    Adresses feedback

--- test/execution-tests/3.0.1_resolveModuleName/test/app.tests.ts
+++ test/execution-tests/3.0.1_resolveModuleName/test/app.tests.ts
@@ -1,8 +1,8 @@
 import * as app from '../src/app';
 
 // We don't actually care about the result of the following operations;
-// what we care about is typescript resolving json as modules
-// allowing this code to compile without errors, hence this:
+// what we care about is typescript resolving modules using our custom
+// logic, allowing this code to compile without errors, hence this:
 // 		"noEmitOnError": true
 // in tsconfig.json
 describe("app", () => {
