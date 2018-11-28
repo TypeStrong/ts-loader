b14d6b4599c3a0399030fbe0158346a7e500786a
John Reilly <johnny_reilly@hotmail.com>
2018-11-28 18:43:37 +0000

    fix compilation errors in tests

--- test/execution-tests/option-context/src/components/deep/deeper/DeeperApp.tsx
+++ test/execution-tests/option-context/src/components/deep/deeper/DeeperApp.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import enhanceWithClickOutside from 'react-click-outside';
 
 const DeeperApp: React.SFC<{ name: string }> = ({ name }) => 
--- test/execution-tests/simpleDependency/test/app.tests.ts
+++ test/execution-tests/simpleDependency/test/app.tests.ts
@@ -1,5 +1,3 @@
-/// <reference path="../typings/index.d.ts" />
-
 import dep = require('../src/dep');
 
 describe("app", () => {
