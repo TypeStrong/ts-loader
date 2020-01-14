22a7227cb6985ca6e44fb0f4900698c2cc5d4c23
John Reilly <johnny_reilly@hotmail.com>
2020-01-14 08:45:55 +0000

    generate 3.7 output

--- test/comparison-tests/stub-new-version.js
+++ test/comparison-tests/stub-new-version.js
@@ -10,8 +10,8 @@ fs.readdirSync(__dirname).forEach(function(test) {
         if (test === 'testLib') return;
         
         // update this manually when a new version comes out
-        var expectedOutput = path.join(testPath, 'expectedOutput-3.5'),
-            newExpectedOutput = path.join(testPath, 'expectedOutput-3.6');
+        var expectedOutput = path.join(testPath, 'expectedOutput-3.6'),
+            newExpectedOutput = path.join(testPath, 'expectedOutput-3.7');
         
         mkdirp.sync(newExpectedOutput);
         copySync(expectedOutput, newExpectedOutput);
