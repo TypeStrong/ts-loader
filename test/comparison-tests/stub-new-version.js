65e4bc57224023b7031bb921164e5ed177a40d91
Johnny Reilly <johnny_reilly@hotmail.com>
2020-05-24 14:34:08 +0100

    generate stubs for transpile as well

--- test/comparison-tests/stub-new-version.js
+++ test/comparison-tests/stub-new-version.js
@@ -1,19 +1,39 @@
+// @ts-check
 const fs = require('fs-extra');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const copySync = require('./copySync');
 
+// update this manually when a new version comes out
+const OLD_VERSION = '3.8';
+const NEW_VERSION = '3.9';
+
 fs.readdirSync(__dirname).forEach(function(test) {
-    var testPath = path.join(__dirname, test);
-    if (fs.statSync(testPath).isDirectory()) {
-        
-        if (test === 'testLib') return;
-        
-        // update this manually when a new version comes out
-        var expectedOutput = path.join(testPath, 'expectedOutput-3.8'),
-            newExpectedOutput = path.join(testPath, 'expectedOutput-3.9');
-        
-        mkdirp.sync(newExpectedOutput);
-        copySync(expectedOutput, newExpectedOutput);
+  const testPath = path.join(__dirname, test);
+  if (fs.statSync(testPath).isDirectory()) {
+    if (test === 'testLib') return;
+
+    const expectedOutput = path.join(testPath, `expectedOutput-${OLD_VERSION}`);
+    const newExpectedOutput = path.join(
+      testPath,
+      `expectedOutput-${NEW_VERSION}`
+    );
+
+    mkdirp.sync(newExpectedOutput);
+    copySync(expectedOutput, newExpectedOutput);
+
+    const expectedTranspileOutput = path.join(
+      testPath,
+      `expectedOutput-${OLD_VERSION}`
+    );
+    const newExpectedTranspileOutput = path.join(
+      testPath,
+      `expectedOutput-${NEW_VERSION}`
+    );
+
+    if (fs.existsSync(expectedTranspileOutput)) {
+      mkdirp.sync(newExpectedTranspileOutput);
+      copySync(expectedTranspileOutput, newExpectedTranspileOutput);
     }
+  }
 });
