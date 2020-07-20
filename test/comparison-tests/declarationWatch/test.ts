729b59bdb7f1d099943257e1bc66b6f38e64d81e
Pig Fang <g-plane@hotmail.com>
2020-07-20 09:53:23 +0800

    make snapshot: declarationWatch

---
+++ test/comparison-tests/declarationWatch/test.ts
@@ -0,0 +1,20 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+import * as utils from '../utils'
+
+test('build', async () => {
+  const compiler = webpack(utils.webpackConfig(path.join(__dirname, 'app.ts')))
+  const memfs = utils.createMemfs()
+
+  const build = await utils.runWatchBuild(memfs, compiler, {
+    iteration: 2,
+    directory: __dirname,
+    path: 'thing.d.ts',
+  })
+  for await (const stats of build) {
+    const bundle = await utils.readFile(memfs, '/bundle.js')
+
+    expect(utils.normalizeBundle(bundle)).toMatchSnapshot('bundle')
+    expect(utils.serializeStats(stats)).toMatchSnapshot('stats')
+  }
+})
