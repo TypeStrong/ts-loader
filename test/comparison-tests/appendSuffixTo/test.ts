3b0b5eca9a30acc820562cfc379497872a1e5f6a
Pig Fang <g-plane@hotmail.com>
2020-07-21 15:18:15 +0800

    make snapshot: appendSuffixTo

---
+++ test/comparison-tests/appendSuffixTo/test.ts
@@ -0,0 +1,46 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+import * as utils from '../utils'
+
+test('build', async () => {
+  const config = utils.webpackConfig(path.join(__dirname, 'index.vue'))
+  config.module.rules = [
+    {
+      test: /\.(?:ts|vue)$/,
+      loader: require.resolve(path.join(process.cwd(), 'dist')),
+      options: {
+        appendTsSuffixTo: [/\.vue$/],
+      }
+    },
+  ]
+  const compiler = webpack(config)
+  const memfs = utils.createMemfs()
+
+  const stats = await utils.runSingleBuild(memfs, compiler)
+  const bundle = await utils.readFile(memfs, '/bundle.js')
+
+  expect(utils.normalizeBundle(bundle)).toMatchSnapshot('bundle')
+  expect(utils.serializeStats(stats)).toMatchSnapshot('stats')
+})
+
+test('transpile only', async () => {
+  const config = utils.webpackConfig(path.join(__dirname, 'index.vue'))
+  config.module.rules = [
+    {
+      test: /\.(?:ts|vue)$/,
+      loader: require.resolve(path.join(process.cwd(), 'dist')),
+      options: {
+        appendTsSuffixTo: [/\.vue$/],
+        transpileOnly: true,
+      },
+    },
+  ]
+  const compiler = webpack(config)
+  const memfs = utils.createMemfs()
+
+  const stats = await utils.runSingleBuild(memfs, compiler)
+  const bundle = await utils.readFile(memfs, '/bundle.js')
+
+  expect(utils.normalizeBundle(bundle)).toMatchSnapshot('bundle')
+  expect(utils.serializeStats(stats)).toMatchSnapshot('stats')
+})
