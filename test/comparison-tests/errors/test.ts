355764a19e6468bb277d91406d08300133dac97b
Pig Fang <g-plane@hotmail.com>
2020-07-22 08:46:56 +0800

    make snapshot: errors

---
+++ test/comparison-tests/errors/test.ts
@@ -0,0 +1,25 @@
+import * as path from 'path'
+import * as webpack from 'webpack'
+import * as utils from '../utils'
+
+test('build', async () => {
+  const compiler = webpack(utils.webpackConfig(path.join(__dirname, 'app.ts')))
+  const memfs = utils.createMemfs()
+
+  const stats = await utils.runSingleBuild(memfs, compiler)
+  const bundle = await utils.readFile(memfs, '/bundle.js')
+
+  expect(utils.normalizeBundle(bundle)).toMatchSnapshot('bundle')
+  expect(utils.serializeStats(stats)).toMatchSnapshot('stats')
+})
+
+test('transpile only', async () => {
+  const compiler = webpack(utils.webpackConfig(path.join(__dirname, 'app.ts'), {transpileOnly:true}))
+  const memfs = utils.createMemfs()
+
+  const stats = await utils.runSingleBuild(memfs, compiler)
+  const bundle = await utils.readFile(memfs, '/bundle.js')
+
+  expect(utils.normalizeBundle(bundle)).toMatchSnapshot('bundle')
+  expect(utils.serializeStats(stats)).toMatchSnapshot('stats')
+})
