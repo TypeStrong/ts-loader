0bd51cf80bdf0b389fca40eb0e4c2905eb1cce83
Pig Fang <g-plane@hotmail.com>
2020-07-25 19:08:36 +0800

    add script

---
+++ test/comparison-tests/runTest.ps1
@@ -0,0 +1,8 @@
+$tests = Get-ChildItem ./test/comparison-tests -Directory | Where-Object {
+  $name = $_.GetDirectories().Name
+  $name -and $name.Contains('__snapshots__')
+}
+
+foreach ($test in $tests) {
+  yarn jest --testRegex="$test/test.ts" --testTimeout=8000
+}
