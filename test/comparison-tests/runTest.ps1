132f4080a89cf8f4458c321ee419882488e01a82
Pig Fang <g-plane@hotmail.com>
2020-07-29 22:55:17 +0800

    remove windows tests script

--- test/comparison-tests/runTest.ps1
+++
@@ -1,8 +0,0 @@
-$tests = Get-ChildItem ./test/comparison-tests -Directory | Where-Object {
-  $name = $_.GetDirectories().Name
-  $name -and $name.Contains('__snapshots__')
-}
-
-foreach ($test in $tests) {
-  yarn jest --testRegex="$test/test.ts" --testTimeout=8000 --colors
-}
