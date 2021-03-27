357322c7b5f25d26b71b34f64c97b05d6897223a
John Reilly <johnny_reilly@hotmail.com>
2021-03-27 07:54:23 +0000

    remove 3.9 test output

--- test/comparison-tests/projectReferencesSymLinksPreserve/expectedOutput-3.9/patch0/lib/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getMeaningOfLife3 = void 0;
-var common_1 = require("common");
-exports.getMeaningOfLife3 = function () { return common_1.getMeaningOfLife2(); };
