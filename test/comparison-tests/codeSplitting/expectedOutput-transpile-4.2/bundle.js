7ebd413a37258d3ae014445bdc90c24a974b1f43
John Reilly <johnny_reilly@hotmail.com>
2021-05-22 07:04:24 +0000

    update codesplitting output

--- test/comparison-tests/codeSplitting/expectedOutput-transpile-4.2/bundle.js
+++ test/comparison-tests/codeSplitting/expectedOutput-transpile-4.2/bundle.js
@@ -246,7 +246,7 @@ eval("\nmodule.exports = 'b';\n\n\n//# sourceURL=webpack:///./b.ts?");
 /******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
 /******/ 				}
 /******/ 			}
-/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			if(runtime) var result = runtime(__webpack_require__);
 /******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 /******/ 			for(;i < chunkIds.length; i++) {
 /******/ 				chunkId = chunkIds[i];
