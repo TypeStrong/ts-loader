83aa5760d901813bfd719ed381f653c18108d751
johnnyreilly <johnny_reilly@hotmail.com>
2021-11-20 07:51:07 +0000

    4.5

--- test/comparison-tests/projectReferencesSymLinks/expectedOutput-transpile-4.3/lib/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getMeaningOfLife = void 0;
-var common_1 = require("common");
-var getMeaningOfLife = function () { return common_1.getMeaningOfLife2(); };
-exports.getMeaningOfLife = getMeaningOfLife;
