9810fb8904b3d38aed6bd91f527e0f99335bcbc7
Homa Wong <homawong@gmail.com>
2022-06-11 00:39:16 -0700

    chore: update TypeScript to 4.7.3
    Just updating the test outputs.
    
    Related to Module not found for TypeScript 4.7 #1463

---
+++ test/comparison-tests/es6codeSplitting/expectedOutput-transpile-4.7/d_ts.bundle.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["d_ts"],{
+
+/***/ "./d.ts":
+/*!**************!*\
+  !*** ./d.ts ***!
+  \**************/
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nexports.default = 'd';\n\n\n//# sourceURL=webpack:///./d.ts?");
+
+/***/ })
+
+}]);
---
+++ test/comparison-tests/projectReferencesMultiple/expectedOutput-transpile-4.7/patch1/utils/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.utils = void 0;
+var common_1 = require("../common");
+function utils() {
+    (0, common_1.common)();
+}
+exports.utils = utils;
