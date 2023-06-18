6f7380b819b04908c4223cf5217c3f0bd4b2be0a
John Reilly <johnny_reilly@hotmail.com>
2023-06-18 13:30:00 +0100

    chore: stub 5.1 test pack (#1616)
    * chore: stub 5.1 test pack
    
    * chore: fix diffs

--- test/comparison-tests/stub-new-version.js
+++ test/comparison-tests/stub-new-version.js
@@ -5,11 +5,11 @@ const mkdirp = require('mkdirp');
 const copySync = require('./copySync');
 
 // BASH script to delete old test output
-// find ./test/comparison-tests -type d -name '*expectedOutput*4.9' -print0 | xargs -0 -I {} /bin/rm -rf "{}"
+// find ./test/comparison-tests -type d -name '*expectedOutput*5.0' -print0 | xargs -0 -I {} /bin/rm -rf "{}"
 
 // update this manually when a new version comes out
-const OLD_VERSION = '4.9';
-const NEW_VERSION = '5.0';
+const OLD_VERSION = '5.0';
+const NEW_VERSION = '5.1';
 
 fs.readdirSync(__dirname).forEach(function(test) {
   const testPath = path.join(__dirname, test);
