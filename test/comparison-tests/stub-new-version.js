62b99b9a977b5015c6539f12ddc2ddee7901665f
johnnyreilly <johnny_reilly@hotmail.com>
2023-09-20 20:41:07 +0100

    feat: ts 5.2

--- test/comparison-tests/stub-new-version.js
+++ test/comparison-tests/stub-new-version.js
@@ -8,8 +8,8 @@ const copySync = require('./copySync');
 // find ./test/comparison-tests -type d -name '*expectedOutput*5.0' -print0 | xargs -0 -I {} /bin/rm -rf "{}"
 
 // update this manually when a new version comes out
-const OLD_VERSION = '5.0';
-const NEW_VERSION = '5.1';
+const OLD_VERSION = '5.1';
+const NEW_VERSION = '5.2';
 
 fs.readdirSync(__dirname).forEach(function(test) {
   const testPath = path.join(__dirname, test);
