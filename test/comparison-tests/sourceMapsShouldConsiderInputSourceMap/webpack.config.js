39ffa2fc0c465125d0ffded81805e45ca34c1dda
Kai Takac <kai.takac@gmail.com>
2023-09-26 13:35:07 +0200

    add test

---
+++ test/comparison-tests/sourceMapsShouldConsiderInputSourceMap/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'development',
+    entry: './a.ts',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+}
