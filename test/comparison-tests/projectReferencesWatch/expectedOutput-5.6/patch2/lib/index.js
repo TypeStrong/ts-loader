5379bb1fc4c4ea53b14a2c2ba88154fedb7de11e
John Reilly <johnny_reilly@hotmail.com>
2024-09-20 10:30:27 +0100

    Update testpack for TypeScript 5.6 (#1656)
    * feat: ts 5.6 stub
    
    * feat: push
    
    * feat: update test data

---
+++ test/comparison-tests/projectReferencesWatch/expectedOutput-5.6/patch2/lib/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.lib = void 0;
+exports.lib = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+} // Add new number
+;
+;
+//# sourceMappingURL=index.js.map
